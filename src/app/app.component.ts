1f9cae86aac84864669b9fe3689bd420b2151a77
abayoss <abayoss@yahoo.com>
2019-01-20 09:41:54 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jv';
+  onUpdate = false;
+  current = {
+    id : 0,
+    title : '',
+    active : false
+  };
+  courses = [
+    {id: 1, title: 'Laravel', active: true },
+    {id: 2, title: 'ANGULAR', active: true },
+    {id: 3, title: 'JAVAEE', active: false },
+    {id: 4, title: 'SYMFONY', active: true }
+  ];
+
+  addCourse() {
+    this.courses.unshift(this.current);
+    this.current = {
+      id : 0,
+      title : '',
+      active : false
+    };
+  }
+  deleteCourse(course) {
+    if (confirm(`you wanna delete ${course.title}?`)) {
+      const index = this.courses.indexOf(course);
+      this.courses.splice(index, 1);
+    }
+  }
+  editCourse(course) {
+    this.onUpdate = true;
+    this.current = course;
+  }
+  updateCourse() {
+    this.onUpdate = false;
+
+  }
+  tooglePower(course) {
+    course.active = !course.active;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { Content1Component } from './content1/content1.component';
+import { Content2Component } from './content2/content2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    Content1Component,
+    Content2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
